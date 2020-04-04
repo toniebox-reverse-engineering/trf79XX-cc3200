1d5849eddb303c4701399c3f75f2b61f134f4b92
scilor <SciLor@users.noreply.github.com>
2020-04-04 19:08:36 +0200

    Initial Commit

---
+++ Debug/Hardware/spi.d
@@ -0,0 +1,45 @@
+# FIXED
+
+Hardware/spi.obj: ../Hardware/spi.c
+Hardware/spi.obj: ../Hardware/spi.h
+Hardware/spi.obj: ../Hardware/mcu.h
+Hardware/spi.obj: ../Hardware/types.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdio.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_ti_config.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/linkage.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdarg.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h
+Hardware/spi.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdbool.h
+Hardware/spi.obj: ../Hardware/VLO_Library.h
+Hardware/spi.obj: ../Hardware/trf79xxa.h
+Hardware/spi.obj: ../Hardware/spi.h
+Hardware/spi.obj: ../Hardware/uart.h
+
+../Hardware/spi.c: 
+../Hardware/spi.h: 
+../Hardware/mcu.h: 
+../Hardware/types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdio.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_ti_config.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/linkage.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdarg.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdbool.h: 
+../Hardware/VLO_Library.h: 
+../Hardware/trf79xxa.h: 
+../Hardware/spi.h: 
+../Hardware/uart.h: 
---
+++ Debug/main.d
@@ -0,0 +1,83 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/string.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_ti_config.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/linkage.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_types.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_memmap.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_common_reg.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_ints.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/spi.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/mcu.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/types.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdio.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdarg.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h
+main.obj: D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdbool.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/VLO_Library.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/trf79xxa.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/spi.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/uart.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom_map.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom_patch.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/utils.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/prcm.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/interrupt.h
+main.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/example/common/uart_if.h
+main.obj: ../pinmux.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/nfc_app.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso14443a.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/ndef.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso14443b.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso15693.h
+main.obj: D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/felica.h
+
+../main.c: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/string.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_ti_config.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/linkage.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/cdefs.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_types.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_memmap.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_common_reg.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/inc/hw_ints.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/spi.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/mcu.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdio.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdarg.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_types.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/_stdint40.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/machine/_stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/sys/_stdint.h: 
+D:/Programme/TI/CCS/920/ccs/tools/compiler/ti-cgt-arm_19.6.0.STS/include/stdbool.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/VLO_Library.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/trf79xxa.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/spi.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/Hardware/uart.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom_map.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/rom_patch.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/utils.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/prcm.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/interrupt.h: 
+D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/example/common/uart_if.h: 
+../pinmux.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/nfc_app.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso14443a.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/ndef.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso14443b.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/iso15693.h: 
+D:/CCS\ Workspaces/Toniebox/spi_demo/NFC/felica.h: 
