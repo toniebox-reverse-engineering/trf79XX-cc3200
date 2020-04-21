662cda4ab2a3fb0cf935d2fdb6ff8d5d040bcb79
scilor <SciLor@users.noreply.github.com>
2020-04-21 19:23:39 +0200

    Added SPI Contstants

--- Hardware/spi.h
+++ Hardware/spi.h
@@ -43,6 +43,66 @@
 
 #include "mcu.h"
 #include "trf79xxa.h"
+//*****************************************************************************
+// Values that can be passed to SPIConfigSetExpClk() as ulMode parameter
+//*****************************************************************************
+#define SPI_MODE_MASTER    0x00000000
+#define SPI_MODE_SLAVE     0x00000004
+
+//*****************************************************************************
+// Values that can be passed to SPIConfigSetExpClk() as ulSubMode parameter
+//*****************************************************************************
+#define SPI_SUB_MODE_0     0x00000000
+#define SPI_SUB_MODE_1     0x00000001
+#define SPI_SUB_MODE_2     0x00000002
+#define SPI_SUB_MODE_3     0x00000003
+
+
+//*****************************************************************************
+// Values that can be passed to SPIConfigSetExpClk() as ulConfigFlags parameter
+//*****************************************************************************
+#define SPI_SW_CTRL_CS      0x01000000
+#define SPI_HW_CTRL_CS      0x00000000
+#define SPI_3PIN_MODE       0x02000000
+#define SPI_4PIN_MODE       0x00000000
+#define SPI_TURBO_ON        0x00080000
+#define SPI_TURBO_OFF       0x00000000
+#define SPI_CS_ACTIVEHIGH   0x00000000
+#define SPI_CS_ACTIVELOW    0x00000040
+#define SPI_WL_8            0x00000380
+#define SPI_WL_16           0x00000780
+#define SPI_WL_32           0x00000F80
+
+//*****************************************************************************
+// Values that can be passed to SPIFIFOEnable() and SPIFIFODisable()
+//*****************************************************************************
+#define SPI_TX_FIFO        0x08000000
+#define SPI_RX_FIFO        0x10000000
+
+//*****************************************************************************
+// Values that can be passed to SPIDMAEnable() and SPIDMADisable()
+//*****************************************************************************
+#define SPI_RX_DMA         0x00008000
+#define SPI_TX_DMA         0x00004000
+
+//*****************************************************************************
+// Values that can be passed to SPIIntEnable(), SPIIntDiasble(),
+// SPIIntClear() or returned from SPIStatus()
+//*****************************************************************************
+#define SPI_INT_DMATX         0x20000000
+#define SPI_INT_DMARX         0x10000000
+#define SPI_INT_EOW           0x00020000
+#define SPI_INT_WKS           0x00010000
+#define SPI_INT_RX_OVRFLOW    0x00000008
+#define SPI_INT_RX_FULL       0x00000004
+#define SPI_INT_TX_UDRFLOW    0x00000002
+#define SPI_INT_TX_EMPTY      0x00000001
+
+//*****************************************************************************
+// Values that can be passed to SPITransfer()
+//*****************************************************************************
+#define SPI_CS_ENABLE  0x00000001
+#define SPI_CS_DISABLE 0x00000002
 
 //===============================================================
 
