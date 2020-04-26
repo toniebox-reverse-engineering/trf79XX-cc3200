b94b68dd711b68d65fa54b26b1d4595dd68425c9
SciLor <>
2020-04-26 19:08:33 +0200

    Remove Builds

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/driverlib/ccs/Release/driverlib.a" -llibc.a
-
--- Debug/startup_ccs.d
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-startup_ccs.obj: D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/example/common/startup_ccs.c
-
-D:/Programme/TI/CC3200SDK/1.5.0/cc3200-sdk/example/common/startup_ccs.c: 
